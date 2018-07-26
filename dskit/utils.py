ec6aa75d361b010af38f10916ac6f982e9e7cffe
SeverinLangberg <Langberg91@gmail.com>
2018-07-26 22:41:57 +0200

    Added utilities module with score stats function

---
+++ dskit/utils.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# utils.py
+#
+# This module is part of dskit.
+#
+
+"""
+The dskit utilities module.
+"""
+
+__author__ = 'Severin E. R. Langberg'
+__email__ = 'Langberg91@gmail.no'
+
+
+import numpy as np
+
+
+def score_stats(scores):
+    # Computes mean and standard deviation of score values.
+
+    return np.mean(scores, axis=1), np.std(scores, axis=1)
