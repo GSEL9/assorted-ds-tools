727c0ab9b61640906ac0636f69d6cc5dfef1bddc
SeverinLangberg <Langberg91@gmail.com>
2018-07-28 18:15:12 +0200

    Added train test scale tests

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -24,17 +24,12 @@ from pytest import approx
 from dstools import prep
 
 
-def train_test_scaling():
-
-    pass
-
-
 class TestStandardizer:
 
     # Arbitrary initiator to random number generator.
     SEED = 123
 
-    THRESH = 1e-13
+    THRESH = 1e-10
 
     @pytest.fixture
     def data(self):
@@ -95,6 +90,68 @@ class TestStandardizer:
         data_direct_trans = scaler().fit_transform(data)
 
         assert data_direct_trans == approx(data_trans, rel=self.THRESH)
+
+
+class TestTrainTestScaling:
+
+    # Arbitrary initiator to random number generator.
+    SEED = 123
+
+    THRESH = 1e-10
+
+    SPLIT_SIZE = 0.2
+
+    @pytest.fixture
+    def data(self):
+
+        np.random.seed(self.SEED)
+
+        num_samples, num_features = 100, 10
+        X = np.random.random((num_samples, num_features))
+        y = np.random.random((num_samples, 1))
+
+        return X, y
+
+    def test_mean(self, data):
+
+        # NOTE: Test data is standardized from train data which excludes mean
+        # and std test properties.
+
+        X, y = data
+        X_train, _, _, _ = prep.train_test_scaling(
+            X, y, self.SPLIT_SIZE, self.SEED
+        )
+        train_avg = np.mean(X_train, axis=0)
+        assert train_avg == approx(np.zeros((train_avg.shape)), rel=self.THRESH)
+
+    def test_std(self, data):
+
+        # NOTE: Test data is standardized from train data which excludes mean
+        # and std test properties.
+
+        X, y = data
+        X_train, _, _, _ = prep.train_test_scaling(
+            X, y, self.SPLIT_SIZE, self.SEED
+        )
+        train_std = np.std(X_train, axis=0)
+        assert train_std == approx(np.ones((train_std.shape)), rel=self.THRESH)
+
+    def test_split(self, data):
+
+        X, y = data
+        org_num_rows, org_num_cols = X.shape
+
+        X_train, X_test, y_train, y_test = prep.train_test_scaling(
+            X, y, self.SPLIT_SIZE, self.SEED
+        )
+        assert X_train.shape[1] == org_num_cols
+        assert X_test.shape[1] == org_num_cols
+        
+        joined_X_rows = int(X_train.shape[0]) + int(X_test.shape[0])
+        assert joined_X_rows == org_num_rows
+
+        joined_y_rows = int(y_train.shape[0]) + int(y_test.shape[0])
+        assert joined_y_rows == org_num_rows
 
 
 class TestDiscardOutliers:
