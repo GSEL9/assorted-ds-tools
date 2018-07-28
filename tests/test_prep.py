090eed84c669cd51ece8e348ade9b1b520153ea4
SeverinLangberg <Langberg91@gmail.com>
2018-07-28 18:33:22 +0200

    Added test skeleton to discard outliers tool

--- tests/test_prep.py
+++ tests/test_prep.py
@@ -11,7 +11,7 @@ Testing of data pre-processing tools.
 
 __author__ = 'Severin E. R. Langberg'
 __email__ = 'Langberg91@gmail.no'
-__status__ = 'Passed {}'
+__status__ = 'Passed 07.28.2018'
 
 
 import pytest
@@ -20,6 +20,7 @@ import numpy as np
 import pandas as pd
 
 from . import mocking
+
 from pytest import approx
 from dstools import prep
 
@@ -29,7 +30,7 @@ class TestStandardizer:
     # Arbitrary initiator to random number generator.
     SEED = 123
 
-    THRESH = 1e-10
+    THRESH = 1.e-10
 
     @pytest.fixture
     def data(self):
@@ -97,7 +98,7 @@ class TestTrainTestScaling:
     # Arbitrary initiator to random number generator.
     SEED = 123
 
-    THRESH = 1e-10
+    THRESH = 1.e-10
 
     SPLIT_SIZE = 0.2
 
@@ -136,7 +137,7 @@ class TestTrainTestScaling:
         train_std = np.std(X_train, axis=0)
         assert train_std == approx(np.ones((train_std.shape)), rel=self.THRESH)
 
-    def test_split(self, data):
+    def test_split_shapes(self, data):
 
         X, y = data
         org_num_rows, org_num_cols = X.shape
@@ -146,14 +147,72 @@ class TestTrainTestScaling:
         )
         assert X_train.shape[1] == org_num_cols
         assert X_test.shape[1] == org_num_cols
-        
+
         joined_X_rows = int(X_train.shape[0]) + int(X_test.shape[0])
         assert joined_X_rows == org_num_rows
 
         joined_y_rows = int(y_train.shape[0]) + int(y_test.shape[0])
         assert joined_y_rows == org_num_rows
 
+    def test_split_size(self, data):
+
+        X, y = data
+        org_num_rows, org_num_cols = X.shape
+
+        X_train, X_test, y_train, y_test = prep.train_test_scaling(
+            X, y, self.SPLIT_SIZE, self.SEED
+        )
+        assert int(X_test.shape[0]) / int(X.shape[0]) == self.SPLIT_SIZE
+        assert int(X_train.shape[0]) / int(X.shape[0]) == 1 - self.SPLIT_SIZE
+
 
 class TestDiscardOutliers:
 
-    pass
+    FIELD = 'logerror'
+    MIN_VAL = -0.4
+    MAX_VAL = 0.4
+
+    THRESH = 1.e-10
+
+    @pytest.fixture
+    def data(self):
+
+        return None
+
+    @pytest.fixture
+    def discarder(self):
+
+        _filter = prep.DiscardOutliers(
+            field=self.FIELD, min_val=self.MIN_VAL, max_val=self.MAX_VAL
+        )
+
+        return _filter
+
+    def test_init(self, discarder):
+
+        assert isinstance(discarder, prep.DiscardOutliers)
+
+    def test_fit(self, discarder, data):
+
+        #discarder().fit(data)
+        pass
+
+    def test_query(self, discarder, data):
+
+        #discarder.fit(data)
+        #assert isinstance(discarder.query, str)
+        pass
+
+    def test_min_val(self, discarder):
+
+        assert discarder.min_val == approx(self.MIN_VAL, rel=self.THRESH)
+
+    def test_max_val(self, discarder):
+
+        assert discarder.max_val == approx(self.MAX_VAL, rel=self.THRESH)
+
+    def test_transform(self, discarder, data):
+
+        #discarder.fit(data)
+        #discarder.transform(data)
+        pass
