e12c47d72f3f84682240ce2102bd41d4f3ed273e
SeverinLangberg <Langberg91@gmail.com>
2018-07-28 15:34:06 +0200

    added base estimator for stacking

---
+++ dstools/ensemble.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# ensemble.py
+#
+# This module is part of dstools.
+#
+
+"""
+Tools for comparing algorithms and hyperparameter optimization.
+"""
+
+__author__ = 'Severin E. R. Langberg'
+__email__ = 'Langberg91@gmail.no'
--- dstools/utils.py
+++ dstools/utils.py
@@ -20,3 +20,28 @@ def score_stats(scores):
     # Computes mean and standard deviation of score values.
 
     return np.mean(scores, axis=1), np.std(scores, axis=1)
+
+
+class BaseEstimator:
+    """An estimator wrapper."""
+
+    # NOTE: Used in stacking.
+    def __init__(self, estimator):
+
+        self.estimator = estimator
+
+    def train(self, X_train, y_train):
+
+        self.estimator.fit(X_train, y_train)
+
+    def predict(self, X):
+
+        return self.estimator.predict(X)
+
+    def fit(self, X, y):
+
+        return self.estimator.fit(X, y)
+
+    def feature_importances(self, X, y):
+
+        print(self.estimator.fit(X, y).feature_importances_)
