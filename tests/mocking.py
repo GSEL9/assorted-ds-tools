6291a5087cd14d59863f8cdc0e41500ec61ff314
SeverinLangberg <Langberg91@gmail.com>
2018-07-28 19:19:25 +0200

    Added mock encoder skeleton

--- tests/mocking.py
+++ tests/mocking.py
@@ -32,3 +32,26 @@ class MockScaler:
     def transform(self, X):
 
         return np.divide(X, self._feature_std)
+
+
+class MockEncoder:
+    """Mocking a feature encoder object."""
+
+    def __init__(self):
+
+        self.old_keys = None
+        self.new_keys = None
+
+    def fit(self, X, y=None, **kwargs):
+
+        pass
+
+    def transform(self, X):
+
+        pass
+
+    def fit_transform(self, X, y=None, **kwargs):
+
+        self.fit(X, y=y, **kwargs)
+
+        return self.transform(X)
