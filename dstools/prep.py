94b070c1c178ce6a940129b908474e138d5e5f32
SeverinLangberg <Langberg91@gmail.com>
2018-07-29 14:32:15 +0200

    Refactoring remove outliser to remove outliers based on percentiles

--- dstools/prep.py
+++ dstools/prep.py
@@ -25,71 +25,39 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-# QUESTION: Rename (e.g. scaler)?
-class Standardizer:
-    """Standardize feature data by subtracting mean and dividing by feature
-    standard deviation."""
+class FeatureScaling:
+    """Scale feature data."""
 
     # NOTE: Fit standard scaler to training data to learn training data
     # parameters. Transform both training data and test data with training
     # data parameters. Thus, standardized training and test data are comparable.
 
+    # NOTE: The objective functions of some learning algorithms objective will
+    # not work properly without normalization.
+
     def __init__(self, scaler=StandardScaler, **kwargs):
 
-        self._scaler = scaler(**kwargs)
+        self.scaler = scaler(**kwargs)
 
     def fit(self, X, y=None, **kwargs):
 
-        self._scaler.fit(X)
+        self.scaler.fit(X)
 
         return self
 
-    def transform(self, X):
+    def transform(self, X, y=None, **kwargs):
 
-        return self._scaler.transform(X)
+        return self.scaler.transform(X)
 
     def fit_transform(self, X, y=None, **kwargs):
 
         self.fit(X, y=y, **kwargs)
 
-        return self.transform(X)
-
-
-# QUESTION: Rename (e.g. train_test_scale/split_and_scale/split_and_standardize
-# /split_center_scale)?
-def train_test_scaling(X, y, test_size, random_state):
-    """Split original feature data into training and test splits including
-    standardization.
-
-    Args:
-        X (array-like): An (n x m) array of feature samples.
-        y (array-like): An (n x 1) array of target samples.
-        test_size (float): The fraction of data used in validation.
-        random_state (int): The random number generator intiator.
-
-    Returns:
-        (tuple): Standardized training and test sets of feature and target
-            data.
-
-    """
-
-    # NOTE: Should be function not class since dependent of random numer
-    # intiator and thus cannot be included in pipeline.
-
-    # Generate training and test sets.
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=test_size, random_state=random_state
-    )
-    # Standardize feature data.
-    scaler = Standardizer()
-    X_train_std = scaler.fit_transform(X_train)
-    X_test_std = scaler.transform(X_test)
-
-    return X_train_std, X_test_std, y_train, y_test
+        return self.transform(X, y=None, **kwargs)
 
 
 class Clone(BaseEstimator, TransformerMixin):
-    """Clone a dataset."""
+    """Clone dataset."""
 
     def __init__(self):
 
@@ -115,84 +83,138 @@ class Clone(BaseEstimator, TransformerMixin):
         return self.transform(X)
 
 
-# ERROR: Cannot query with numpy array. Must convert data to correct dtype.
-class DiscardOutliers(BaseEstimator, TransformerMixin):
-    """Remove outliers based on minimum and maximum criteria of log error
-    value.
+class FeatureImputer(BaseEstimator, TransformerMixin):
+    """Impute missing values by specified fill value or method.
+
+    Args:
+        method (str, {mean, median, most_frequent}): The imputation strategy.
 
     Attributes:
-        query (str):
-        min_val (float): The minimum field value.
-        max_val (float): The maximum field value.
+        targets (list of str): The labels of all features containing the value
+            to impute.
 
     """
 
-    def __init__(self, field='logerror', min_val=-0.4, max_val=0.4):
+    def __init__(self, method='mean', impute_val=np.nan, imputer=None):
 
-        self.field = field
-        self.min_val = min_val
-        self.max_val = max_val
+        self.method = method
+        self.impute_val = impute_val
 
-        self.query = None
+        if imputer is None:
+            self.imputer = Imputer(
+                missing_values=self.impute_val, strategy=self.method, axis=1
+            )
+        else:
+            self.imputer = imputer
 
-    def fit(self, X, y=None):
+        # NOTE: Variable set with instace.
+        self.targets = None
+
+    def fit(self, X, y=None, **kwargs):
+
+        # Determine which features that will be imputed.
+        self.targets = []
+        for feature in X.columns:
+            if np.any(X[feature].isnull()):
+                self.targets.append(feature)
+
+        return self
+
+    def transform(self, X, y=None, **kwargs):
+
+        # TODO: Type checking
+
+        data = X.copy()
+        for feature in self.targets:
+            feature_data = np.atleast_2d(data[feature].values)
+            data[feature] = self.imputer.fit_transform(feature_data)[0]
+
+        return data
+
+    def fit_transform(self, X, y=None, **kwargs):
+
+        self.fit(X, y=y, **kwargs)
+
+        return self.transform(X, y=y, **kwargs)
+
+
+class RemoveOutliers(BaseEstimator, TransformerMixin):
+    """Remove outliers from dataset using percentiles.
+
+    Attributes:
+        quantiles (pandas.DataFrame): The feature values considered outside the
+            boundaries of the percentiles.
+
+    """
+
+    def __init__(self, low=0.05, high=0.95):
+
+        self.low = low
+        self.high = high
+
+        # NOTE: Variable set with instance.
+        self.quantiles = None
+
+    def fit(self, X, y=None, **kwargs):
         """Define query that determines outliers."""
 
-        self.query = '{field} > {min_val} and {field} < {max_val}'.format(
-            field=self.field,
-            min_val=self.min_val,
-            max_val=self.max_val
-        )
+        self.quantiles = X.quantile([self.low, self.high])
+
         return self
 
-    # ERROR:
-    def transform(self, X):
+    def transform(self, X, y=None, **kwargs):
         """Remove outliers from data."""
 
-        return X.query(self.query)
+        def quantile_filter(x):
+
+            lower_limit = self.quantiles.loc[self.low, x.name]
+            upper_limit = self.quantiles.loc[self.high, x.name]
+
+            return x[(x > lower_limit) & (x < upper_limit)]
+
+        return X.apply(quantile_filter, axis=0)
+
+    def fit_transform(self, X, y=None, **kwargs):
+
+        self.fit(X, y=y, **kwargs)
+
+        return self.transform(X, y=y, **kwargs)
 
 
-# WIP:
-class ReplaceNans(BaseEstimator, TransformerMixin):
-    """Replace NaN values by specified fill value or method."""
 
-    def __init__(self, fill_val=-1, features=[], method=None):
+def train_test_scaling(X, y, test_size=0.2, random_state=None, scaler=None):
+    """Split original feature data into training and test splits including
+    standardization.
 
-        self.fill_val = fill_val
-        self.features = features
-        self.method = method
+    Args:
+        X (array-like): An (n x m) array of feature samples.
+        y (array-like): An (n x 1) array of target samples.
+        test_size (float): The fraction of data used in validation.
+        scaler (object): A feature data scaler object.
+        random_state (int): The random number generator intiator.
 
-        self._data = None
+    Returns:
+        (tuple): Standardized training and test sets of feature and target
+            data.
 
-    def fit(self, X, y=None):
+    """
 
-        self._data = check_array(X)
+    # NOTE: Should be function not class since dependent of random numer
+    # intiator and thus cannot be included in pipeline.
 
-        if self.features is None:
-            self.features = self._data.columns
+    # Generate training and test sets.
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state
+    )
+    # Scale feature data with default parameters.
+    if scaler is None:
+        _scaler = FeatureScaling()
+    else:
+        _scaler = FeatureScaling(scaler=scaler)
+    X_train_std = _scaler.fit_transform(X_train)
+    X_test_std = _scaler.transform(X_test)
 
-        return self
-
-    def transform(self):
-
-        if self.method is None:
-            self._data.fillna(self.fill_val)
-
-        elif self.method == 'mean':
-            for feature in self.features:
-                filled = self._data[feature].fillna(self._data[feature].mean)
-                self._data[feature] = filled
-
-        elif self.method == 'std':
-            for feature in self.features:
-                filled = self._data[feature].fillna(self._data[feature].std)
-                self._data[feature] = filled
-
-        else:
-            raise NotImplementedError('Not implemented fill method `{}`'
-                                      ''.format(self.method))
-
-        return self._data
+    return X_train_std, X_test_std, y_train, y_test
 
 
 # WIP:
@@ -278,6 +300,7 @@ if __name__ == '__main__':
         * Mimick LabelEncoder in mock and complete tests.
     """
 
+    import pandas as pd
     from pytest import approx
 
     def data(seed=123):
@@ -290,7 +313,17 @@ if __name__ == '__main__':
 
         return X, y
 
-    data, _ = data()
+    #data, _ = data()
+
+    np.random.seed(123)
+    df = pd.DataFrame(np.random.random((10, 4)))
+
+    filter1 = RemoveOutliers()
+    filter1.fit(df)
+    df_clean = filter1.transform(df)
+
+    filter2 = RemoveOutliers()
+    df_clean2 = filter2.fit_transform(df)
 
 
     class MockEncoder:
